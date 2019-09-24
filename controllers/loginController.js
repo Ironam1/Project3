c955cd4f2ccca96bf687aded13d9d8f5856fe36e
Zach Dunn <zfdunn@comcast.net>
2019-09-23 22:02:25 -0400

    created all individual controller files

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,5 @@
 const db = require("../models");
-
 // Defining methods for the controller
-
 module.exports = {  
 //-------------------------------------------------
 findAll: function(req, res) {
