bb008646ba23253b0beba0a701b4144470093294
Zach Dunn <zfdunn@comcast.net>
2019-09-28 13:36:54 -0400

    git routes/api/user.js created

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,6 +15,32 @@ const User = require("../../models/user");
 router.post("/register",(req,res) => {
 //form validation
     const { errors, isValid } = validateRegisterInput(req.body);
-//
+//check validation
+    if (!isValid){ 
+        return res.status(400).json(errors);
+    }
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = newUser({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+// hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) ==> {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
 
 }
